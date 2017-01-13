112be03eeb6576d591c2672458c08c476bf12b87
pzhang <pengzh.leo@gmail.com>
2017-01-13 16:50:45 +0800

    增加一个简单的adapter

---
+++ base/src/main/java/com/being/base/ui/widget/recyclerview/AbstractViewHolder.java
@@ -0,0 +1,20 @@
+package com.being.base.ui.widget.recyclerview;
+
+import android.support.annotation.LayoutRes;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import com.chad.library.adapter.base.BaseViewHolder;
+
+/**
+ * Created by zhangpeng on 17/1/13.
+ */
+public abstract class AbstractViewHolder<T> extends BaseViewHolder {
+
+    public AbstractViewHolder(ViewGroup parent, @LayoutRes int id) {
+        super(LayoutInflater.from(parent.getContext()).inflate(id, parent));
+
+    }
+
+    public abstract void bindDate(T data);
+}
