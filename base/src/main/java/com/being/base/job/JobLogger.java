ae3bc73938c606dabe43aef5c4b250c9826471ca
pzhang <pengzh.leo@gmail.com>
2017-01-20 16:15:03 +0800

    日志逻辑fix

--- base/src/main/java/com/being/base/job/JobLogger.java
+++ base/src/main/java/com/being/base/job/JobLogger.java
@@ -25,28 +25,28 @@ public class JobLogger implements CustomLogger {
 
     @Override
     public void d(String text, Object... args) {
-        if (mLogLevel >= Log.DEBUG) {
+        if (mLogLevel <= Log.DEBUG) {
             NHLog.v(text, args);
         }
     }
 
     @Override
     public void e(Throwable t, String text, Object... args) {
-        if (mLogLevel >= Log.ERROR) {
+        if (mLogLevel <= Log.ERROR) {
             NHLog.e(text, t, args);
         }
     }
 
     @Override
     public void e(String text, Object... args) {
-        if (mLogLevel >= Log.ERROR) {
+        if (mLogLevel <= Log.ERROR) {
             NHLog.e(text);
         }
     }
 
     @Override
     public void v(String text, Object... args) {
-        if (mLogLevel >= Log.VERBOSE) {
+        if (mLogLevel <= Log.VERBOSE) {
             NHLog.v(text, args);
         }
     }
