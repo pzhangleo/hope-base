c4e5dfccc6bf9dc82b985624584337d01c0fa5cd
pzhang <pengzh.leo@gmail.com>
2017-03-08 15:11:06 +0800

    fix lint error

--- base/src/main/java/com/being/base/ui/widget/PagerIndicator/TabPageIndicator.java
+++ base/src/main/java/com/being/base/ui/widget/PagerIndicator/TabPageIndicator.java
@@ -20,12 +20,12 @@ import android.content.Context;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.support.v7.widget.AppCompatTextView;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.HorizontalScrollView;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import com.being.base.R;
 
@@ -260,7 +260,7 @@ public class TabPageIndicator extends HorizontalScrollView implements PageIndica
         mListener = listener;
     }
 
-    private class TabView extends TextView {
+    private class TabView extends AppCompatTextView {
         private int mIndex;
 
         public TabView(Context context) {
