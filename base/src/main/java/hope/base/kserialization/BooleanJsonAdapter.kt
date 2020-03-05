56d4f5cee527e9300053f875ed063bc5db47f03d
pzhang <pengzh.leo@gmail.com>
2020-03-05 21:24:13 +0800

    fix kserialization bug

--- base/src/main/java/hope/base/kserialization/BooleanJsonAdapter.kt
+++ base/src/main/java/hope/base/kserialization/BooleanJsonAdapter.kt
@@ -1,19 +1,18 @@
 package hope.base.kserialization
 
 import kotlinx.serialization.*
-import kotlinx.serialization.internal.StringDescriptor
 
 class BooleanJsonAdapter : KSerializer<Boolean> {
     override val descriptor: SerialDescriptor
-        get() = StringDescriptor.withName("BooleanJsonAdapter")
+        get() = PrimitiveDescriptor("BooleanJsonAdapter", PrimitiveKind.STRING)
 
     override fun deserialize(decoder: Decoder): Boolean {
         return decoder.decodeInt() > 0
     }
 
-    override fun serialize(encoder: Encoder, obj: Boolean) {
+    override fun serialize(encoder: Encoder, value: Boolean) {
         encoder.encodeString(
-                if (obj) {
+                if (value) {
                     "1"
                 } else {
                     "0"
