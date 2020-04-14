27d98554b9339644e8e104c57bae513231e660e5
pzhang <pengzh.leo@gmail.com>
2020-04-14 13:11:25 +0800

    add null check for Digest extensions

--- base/src/main/java/hope/base/extensions/DigestExtensions.kt
+++ base/src/main/java/hope/base/extensions/DigestExtensions.kt
@@ -3,22 +3,22 @@ package hope.base.extensions
 import android.util.Base64
 import hope.base.utils.codec.digest.DigestUtils
 
-fun String.md5Hex(): String {
-    if (this.isEmpty()) {
+fun String?.md5Hex(): String {
+    if (this.isNullOrEmpty()) {
         return ""
     }
     return DigestUtils.md5Hex(this)
 }
 
-fun String.sha256Hex(): String {
-    if (this.isEmpty()) {
+fun String?.sha256Hex(): String {
+    if (this.isNullOrEmpty()) {
         return ""
     }
     return DigestUtils.sha256Hex(this)
 }
 
-fun String.base64(flag: Int = Base64.URL_SAFE): String {
-    if (this.isEmpty()) {
+fun String?.base64(flag: Int = Base64.URL_SAFE): String {
+    if (this.isNullOrEmpty()) {
         return ""
     }
     return Base64.encodeToString(this.toByteArray(Charsets.UTF_8), flag)
