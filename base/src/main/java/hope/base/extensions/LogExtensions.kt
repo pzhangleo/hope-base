c9f3d9ce41feebf234e0afb8618cb6d787810f99
pzhang <pengzh.leo@gmail.com>
2020-04-24 16:05:32 +0800

    fix log tag npe

--- base/src/main/java/hope/base/extensions/LogExtensions.kt
+++ base/src/main/java/hope/base/extensions/LogExtensions.kt
@@ -3,17 +3,33 @@ package hope.base.extensions
 import hope.base.log.ZLog
 
 fun String.logd(tag: String? = null) {
-    ZLog.tag(tag).d(this)
+    if (tag.isNullOrEmpty()) {
+        ZLog.d(this)
+    } else {
+        ZLog.tag(tag).d(this)
+    }
 }
 
 fun String.logv(tag: String? = null) {
-    ZLog.tag(tag).v(this)
+    if (tag.isNullOrEmpty()) {
+        ZLog.v(this)
+    } else {
+        ZLog.tag(tag).v(this)
+    }
 }
 
 fun String.logi(tag: String? = null) {
-    ZLog.tag(tag).i(this)
+    if (tag.isNullOrEmpty()) {
+        ZLog.i(this)
+    } else {
+        ZLog.tag(tag).i(this)
+    }
 }
 
 fun String.loge(tag: String? = null, throwable: Throwable? = null) {
-    ZLog.tag(tag).e(this, throwable)
+    if (tag.isNullOrEmpty()) {
+        ZLog.e(this, throwable)
+    } else {
+        ZLog.tag(tag).e(this, throwable)
+    }
 }
