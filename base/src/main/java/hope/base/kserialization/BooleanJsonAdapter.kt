8598ca3d992ceef9a164ff96167d31ba7f4ae446
pzhang <pengzh.leo@gmail.com>
2020-05-20 18:44:38 +0800

    update some lib

--- base/src/main/java/hope/base/kserialization/BooleanJsonAdapter.kt
+++ base/src/main/java/hope/base/kserialization/BooleanJsonAdapter.kt
@@ -1,26 +1,26 @@
-package hope.base.kserialization
-
-import kotlinx.serialization.Decoder
-import kotlinx.serialization.Encoder
-import kotlinx.serialization.KSerializer
-import kotlinx.serialization.PrimitiveDescriptor
-import kotlinx.serialization.PrimitiveKind
-import kotlinx.serialization.SerialDescriptor
-
-class BooleanJsonAdapter : KSerializer<Boolean> {
-    override val descriptor: SerialDescriptor
-        get() = PrimitiveDescriptor("BooleanJsonAdapter", PrimitiveKind.STRING)
-
-    override fun deserialize(decoder: Decoder): Boolean {
-        return decoder.decodeInt() > 0
-    }
-
-    override fun serialize(encoder: Encoder, value: Boolean) {
-        encoder.encodeString(
-                if (value) {
-                    "1"
-                } else {
-                    "0"
-                })
-    }
-}
+//package hope.base.kserialization
+//
+//import kotlinx.serialization.Decoder
+//import kotlinx.serialization.Encoder
+//import kotlinx.serialization.KSerializer
+//import kotlinx.serialization.PrimitiveDescriptor
+//import kotlinx.serialization.PrimitiveKind
+//import kotlinx.serialization.SerialDescriptor
+//
+//class BooleanJsonAdapter : KSerializer<Boolean> {
+//    override val descriptor: SerialDescriptor
+//        get() = PrimitiveDescriptor("BooleanJsonAdapter", PrimitiveKind.STRING)
+//
+//    override fun deserialize(decoder: Decoder): Boolean {
+//        return decoder.decodeInt() > 0
+//    }
+//
+//    override fun serialize(encoder: Encoder, value: Boolean) {
+//        encoder.encodeString(
+//                if (value) {
+//                    "1"
+//                } else {
+//                    "0"
+//                })
+//    }
+//}
